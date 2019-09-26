bd68ca5bfb6d0248165d7f755c94b10969d9d39d
kschang <kschang83@gmail.com>
2019-09-26 19:07:29 +0900

    toogle menu 수정

--- src/components/Component_text.js
+++ src/components/Component_text.js
@@ -1,36 +1,38 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './TemplateEditor.css';
 import Prop_text from './Prop_text.js';
 
-const prop_component = (component, firstId, lastId) =>{
-       const comp = document.getElementById(component.ID);
-       const compId = (component.ID).substring(1, (component.ID).length);
-    
-       const compLength = document.getElementsByClassName('compo').length;
-       const first = parseInt(firstId);
-       const last = parseInt(lastId);
 
-        for(var i=first; i<=last; i++){
-            if(i === parseInt(compId)){
+class Component_text extends Component {
+
+    prop_component = (component, firstId, lastId) => {
+        const comp = document.getElementById(component.ID);
+        const compId = (component.ID).substring(1, (component.ID).length);
+
+        const compLength = document.getElementsByClassName('compo').length;
+        const first = parseInt(firstId);
+        const last = parseInt(lastId);
+
+        for (var i = first; i <= last; i++) {
+            if (i === parseInt(compId)) {
                 comp.style.backgroundColor = '#e0eaec';
                 comp.style.fontWeight = '550';
                 comp.style.bordercolor = '#909090';
                 comp.style.color = '#20323a';
-            }else{
-                const oth_comp = document.getElementById('C'+i);
+            } else {
+                const oth_comp = document.getElementById('C' + i);
                 oth_comp.style.backgroundColor = 'white';
                 oth_comp.style.fontWeight = '100';
                 oth_comp.style.bordercolor = '#bbb8b8';
                 oth_comp.style.color = '#bbb8b8';
             }
         }
-         ReactDOM.render(<Prop_text info={component}/>, document.getElementById('properties_section'));
-       
-};
+        ReactDOM.render(<Prop_text info={component} />, document.getElementById('properties_section'));
 
-class Component_text extends Component{
-    render(){
+    };
+
+    render() {
         const text_data = this.props.text_data;
         const Id = text_data[0].ID;
         const firstId = Id.substring(1, Id.length);
@@ -39,7 +41,7 @@ class Component_text extends Component{
         const Id2 = text_data[(text_data.length) - 1].ID;
         const lastId = Id2.substring(1, Id2.length);
 
-        const componentList = text_data.map(component => (<button className="compo" id={component.ID} onClick={()=>prop_component(component, firstId, lastId)}> {component.TITLE} ({component.ID}) </button>));
+        const componentList = text_data.map(component => (<button className="compo" key={component.ID} id={component.ID} onClick={() => this.prop_component(component, firstId, lastId)}> {component.TITLE} ({component.ID}) </button>));
         return (
             <div className="compoSection">
                 <div className="compoTitle">
