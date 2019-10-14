50fe8274a6ddd7157b9b694137b36610e228a5eb
kschang <kschang83@gmail.com>
2019-10-14 18:07:26 +0900

    콤포넌트 이름 변경(언더바제외)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './App.css';
-import TemplateEditor_main from './components/TemplateEditor_main.js';
+import TemplateEditorMain from './components/TemplateEditor_main.js';
 
-class App extends Component{
-  render(){
-    return(
-      <TemplateEditor_main>
-      </TemplateEditor_main>
+class App extends Component {
+  render() {
+    return (
+      <TemplateEditorMain>
+      </TemplateEditorMain>
     );
   }
 }
